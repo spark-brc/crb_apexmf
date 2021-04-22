2d13c274e6fa5322a50ed85198ca5d07ab07314f
Seonggyu Park <envpsg@gmail.com>
2021-04-21 21:25:35 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,147 @@
+import logging
+import plotly.express as px
+import streamlit as st
+import os
+import pandas as pd
+import base64
+import numpy as np
+from utils import (
+    read_rch_files, get_variables, viz_biomap, get_sim_obd, get_plot, get_stats_df,
+    filedownload
+)
+import glob
+import utils
+
+pd.options.mode.chained_assignment = None
+
+LINE = """<style>
+.vl {
+  border-left: 2px solid black;
+  height: 100px;
+  position: absolute;
+  left: 50%;
+  margin-left: -3px;
+  top: 0;
+}
+</style>
+
+<div class="vl"></div>"""
+
+
+st.set_page_config(layout="wide", initial_sidebar_state='collapsed')
+st.title('CRB APEX-MODFLOW model performance')
+st.markdown("""
+This app helps analyze CRB APEX-MODFLOW model performance.
+- **Main Python libraries:** base64, pandas, streamlit, plotly, geopandas
+- **Source code:** [github.com/spark-brc/crb_apexmf](https://github.com/spark-brc/crb_apexmf)
+""")
+
+ws_nams, full_paths = utils.get_watershed_list()
+col1, line, col2, col3, col4 = st.beta_columns([0.2, 0.05, 0.1, 0.15, 0.2])
+area = col1.selectbox(
+    "Select Watershed", ws_nams
+    )
+with line:
+    st.markdown(LINE, unsafe_allow_html=True)
+with col2:
+    # st.markdown(
+    #     "<h3 style='text-align: center;'>Simulation period</h3>",
+    #     unsafe_allow_html=True)
+    st.markdown(
+        """
+        ### Simulation period
+        """)
+with col3:
+    st.markdown(
+        """
+        ### Streamgage station (Reach ID)
+        """)
+if area == ws_nams[0]:
+    with col2:
+        st.markdown(
+            """
+            ## 1992 - 2001
+            """)
+        st.markdown("---")
+    with col3:
+        st.markdown(
+            """
+            ## 12, 57, 75
+            """)
+        st.markdown("---")
+if area == ws_nams[1]:
+    with col2:
+        st.markdown(
+            """
+            ## 2000 - 2012
+            """)
+    with col3:
+        st.markdown(
+            """
+            ## 9, 96, 199
+            """)
+if area == ws_nams[2]:
+    with col2:
+        st.markdown(
+            """
+            ## 2010 - 2019
+            """)
+    with col3:
+        # st.markdown(
+        #     "<h4 style='text-align: center;'>66, 102, 133</h4>",
+        #     unsafe_allow_html=True)
+        st.markdown(
+            """
+            ## 66, 102, 133
+            """)
+
+
+def main(df, sims):
+    tdf = st.beta_expander('Simulated and Observed Streamflow for {} APEX-MODFLOW Watershed Model'.format(area))
+    tdf.dataframe(df, height=500)
+    tdf.markdown(utils.filedownload(df), unsafe_allow_html=True)
+    st.plotly_chart(utils.get_plot(df, sims), use_container_width=True)
+    stats_df = utils.get_stats_df(df, sims)
+
+    with col4:
+        st.markdown(
+            """
+            ### Objective Functions
+            """)
+        st.dataframe(stats_df.T)
+    wtdf = st.beta_expander('Simulated and Observed Groundwater Level for {} APEX-MODFLOW Watershed Model'.format(area))
+    mddf = st.beta_expander('{} Model Description'.format(area))
+    # tdf.dataframe(df, height=500)
+        
+@st.cache
+def load_data():
+    if area == ws_nams[0]:
+        sims = [12, 57, 75]
+        obds = ['str_012', 'str_057', 'str_075']
+        stdate = '1/1/1980'
+        time_step ='M'
+        caldate = '1/1/1992'
+        eddate = '12/31/2001'
+    if area == ws_nams[1]:
+        sims = [9, 96, 199]
+        obds = ['sub009', 'sub096', 'sub199']
+        stdate = '1/1/1990'
+        time_step ='M'
+        caldate = '1/1/2000'
+        eddate = '12/31/2012'        
+    if area == ws_nams[2]:
+        sims = [66, 102, 133]
+        obds = ['sub_66','sub_102','sub_133']
+        stdate = '1/1/2000'
+        time_step ='M'
+        caldate = '1/1/2010'
+        eddate = '12/31/2019'        
+    df = get_sim_obd(area, stdate, time_step, sims, obds, caldate, eddate)
+    
+    return df, sims
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.CRITICAL)
+    df, sims = load_data()
+    main(df, sims)
---
+++ utils.py
@@ -0,0 +1,199 @@
+import glob
+import os
+import pandas as pd
+import streamlit as st
+import random
+import geopandas as gpd
+import pyproj
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.offline as offline
+import plotly.express as px
+import plotly.graph_objects as go
+from matplotlib import cm
+import numpy as np
+from hydroeval import evaluator, nse, rmse, pbias
+import base64
+
+
+def get_sim_obd(area, stdate, time_step, sims, obds, caldate, eddate):
+    rch_file = read_rch_files(os.path.join("./resources/watershed", area))
+    df = pd.read_csv(
+                    os.path.join("./resources/watershed", area, rch_file[0]),
+                    delim_whitespace=True,
+                    skiprows=9,
+                    usecols=[0, 1, 8],
+                    names=['idx', 'rchid', 'sim'],
+                    index_col=0)
+    df = df.loc["REACH"]
+    str_obd = pd.read_csv(
+                        os.path.join("./resources/watershed", area, 'streamflow.obd'),
+                        sep=r'\s+', index_col=0, header=0,
+                        parse_dates=True, delimiter="\t",
+                        na_values=[-999, ""]
+                        )
+    str_obd = str_obd[obds]
+    tot_sim = pd.DataFrame()
+    for i in sims:
+        df2 = df.loc[df['rchid'] == int(i)]
+        df2.index = pd.date_range(stdate, periods=len(df2.sim), freq=time_step)
+        df2.rename(columns = {'sim':'sim_{:03d}'.format(i)}, inplace = True)
+        tot_sim = pd.concat([tot_sim, df2.loc[:, 'sim_{:03d}'.format(i)]], axis=1,
+            # sort=False
+            )
+    tot_df = pd.concat([tot_sim, str_obd], axis=1)
+    tot_df.index = pd.to_datetime(tot_df.index).normalize()
+    tot_df = tot_df[caldate:eddate]
+    return tot_df
+
+
+def get_plot(df, sims):
+    fig = go.Figure()
+    colors = (get_matplotlib_cmap('tab10', bins=8))
+    for i in range(len(sims)):
+        fig.add_trace(go.Scatter(
+            x=df.index, y=df.iloc[:, i], name='Reach {}'.format(sims[i]),
+            line=dict(color=colors[i], width=2),
+            legendgroup='Reach {}'.format(sims[i])
+            ))
+    for i in range(len(sims)):
+        fig.add_trace(go.Scatter(
+            x=df.index, y=df.iloc[:, i+len(sims)], mode='markers', name='Observed {}'.format(sims[i]),
+            marker=dict(color=colors[i]),
+            legendgroup='Reach {}'.format(sims[i]),
+            showlegend=False
+            ))
+    # line_fig = px.line(df, height=500, width=1200)
+    fig.update_layout(
+        # showlegend=False,
+        plot_bgcolor='white',
+        height=600,
+        # width=1200
+    )
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgray', title='')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgray', title='Stream Discharge (m<sup>3</sup>/s)')
+    fig.update_layout(legend=dict(
+        yanchor="top",
+        y=1.0,
+        xanchor="center",
+        x=0.5,
+        orientation="h",
+        title='',
+    ))
+    fig.update_traces(marker=dict(size=10, opacity=0.5,
+                                line=dict(width=1,
+                                            color='white')
+                                            ),
+                    selector=dict(mode='markers'))
+    return fig
+
+
+def get_stats(df):
+    df_stat = df.dropna()
+    sim = df_stat.iloc[:, 0].to_numpy()
+    obd = df_stat.iloc[:, 1].to_numpy()
+    df_nse = evaluator(nse, sim, obd)[0]
+    df_rmse = evaluator(rmse, sim, obd)[0]
+    df_pibas = evaluator(pbias, sim, obd)[0]
+    r_squared = (
+        ((sum((obd - obd.mean())*(sim-sim.mean())))**2)/
+        ((sum((obd - obd.mean())**2)* (sum((sim-sim.mean())**2))))
+        )
+    return df_nse, df_rmse, df_pibas, r_squared
+
+
+def get_stats_df(df, sims):
+    stats_df = pd.DataFrame()
+    for i in range(len(sims)):
+        exdf = df.iloc[:, [i, i+len(sims)]]
+        df_list = get_stats(exdf)
+        stat_series = pd.Series(['{:.2f}'.format(x) for x in df_list], name='Reach {}'.format(sims[i]))
+        stats_df = pd.concat([stats_df, stat_series], axis=1)
+    stats_df.index = ['NSE', 'RMSE', 'PBIAS', 'R-squared']
+    return stats_df
+
+
+def get_watershed_list():
+    ws_nams = [f.name for f in os.scandir("./resources/watershed") if f.is_dir()]
+    full_paths = [f.path for f in os.scandir("./resources/watershed") if f.is_dir()]
+    return ws_nams, full_paths
+
+
+def viz_biomap():
+    subdf = gpd.read_file("./resources/subs1.shp")
+    subdf.to_crs(pyproj.CRS.from_epsg(4326), inplace=True)
+    subdf = subdf[['Subbasin', 'geometry']]
+    subdf['geometry'] = subdf['geometry'].convex_hull
+    tt = gpd.GeoDataFrame()
+    for i in subdf.index:
+        df = gpd.GeoDataFrame()
+        df['time']= [str(x)[:-9] for x in pd.date_range('1/1/2000', periods=12, freq='M')]
+        # df['time'] = [str(x) for x in range(2000, 2012)]
+        df['Subbasin'] = 'Sub{:03d}'.format(i+1)
+        df['geometry'] = subdf.loc[i, 'geometry']
+        df['value'] = [random.randint(0,12) for i in range(12)]
+        tt = pd.concat([tt, df], axis=0)   
+    tt.index = tt.Subbasin 
+    mfig = px.choropleth(tt,
+                    geojson=tt.geometry,
+                    locations=tt.index,
+                    color="value",
+                    #    projection="mercator"
+                    animation_frame="time",
+                    range_color=(0, 12),
+                    )
+    mfig.update_geos(fitbounds="locations", visible=False)
+    mfig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    offline.plot(mfig, auto_open=True, image = 'png', image_filename="map_us_crime_slider" ,image_width=2000, image_height=1000, 
+                filename='tt.html', validate=True)
+
+    st.plotly_chart(mfig, use_container_width=True)
+
+
+def read_rch_files(wd):
+    # Find .dis file and read number of rows, cols, x spacing, and y spacing (not allowed to change)
+    rch_files = []
+    for filename in glob.glob(wd+"/*.RCH"):
+        rch_files.append(os.path.basename(filename))
+    return rch_files
+
+
+def get_variables(wd, rch_file):
+    columns = pd.read_csv(
+                        os.path.join(wd, rch_file),
+                        delim_whitespace=True,
+                        skiprows=8,
+                        nrows=1,
+                        header=None
+                        )
+
+    col_lst = columns.iloc[0].tolist()
+    col_lst.insert(2, 'YEAR')
+    col_dic = dict((i, j) for i, j in enumerate(col_lst))
+    keys = [x for x in range(0, 39)]
+    col_dic_f = {k: col_dic[k] for k in keys}
+    rch_vars = list(col_dic_f.values())
+    rch_vars = list(col_dic_f.values())
+    return rch_vars
+
+
+# def get_subnums(wd, rch_file)
+def get_matplotlib_cmap(cmap_name, bins, alpha=1):
+    if bins is None:
+        bins = 10
+    cmap = cm.get_cmap(cmap_name)
+    h = 1.0 / bins
+    contour_colour_list = []
+
+    for k in range(bins):
+        C = list(map(np.uint8, np.array(cmap(k * h)[:3]) * 255))
+        contour_colour_list.append('rgba' + str((C[0], C[1], C[2], alpha)))
+
+    C = list(map(np.uint8, np.array(cmap(bins * h)[:3]) * 255))
+    contour_colour_list.append('rgba' + str((C[0], C[1], C[2], alpha)))
+    return contour_colour_list
+
+def filedownload(df):
+    csv = df.to_csv(index=True)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="dataframe.csv">Download CSV File</a>'
+    return href
